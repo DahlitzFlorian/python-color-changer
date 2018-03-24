bc5fc2b0867e63e9543c41501beedc77b813d7d7
Florian Dahlitz <f2dahlitz@freenet.de>
2018-03-24 14:13:30 +0100

    added red-green-changer

---
+++ app.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+
+def red_green(image):
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            image[i][j] = np.array([image[i][j][0], image[i][j][2],
+                                    image[i][j][1]])
+
+    return image
