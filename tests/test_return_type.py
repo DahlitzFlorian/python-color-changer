c2e554155242db9ec874a1c5e7f39296c722e501
Florian Dahlitz <f2dahlitz@freenet.de>
2018-03-27 20:36:53 +0200

    applied PEP8 to tests

--- tests/test_return_type.py
+++ tests/test_return_type.py
@@ -3,12 +3,12 @@ import numpy as np
 
 
 def test_return_red_green():
-    assert type(red_green(np.ndarray((360, 680, 3)))) == type(np.ndarray((100, 100, 3)))
+    assert isinstance(red_green(np.ndarray((360, 680, 3))), np.ndarray)
 
 
 def test_return_green_blue():
-    assert type(green_blue(np.ndarray((360, 680, 3)))) == type(np.ndarray((100, 100, 3)))
+    assert isinstance(green_blue(np.ndarray((360, 680, 3))), np.ndarray)
 
 
 def test_return_blue_red():
-    assert type(blue_red(np.ndarray((360, 680, 3)))) == type(np.ndarray((100, 100, 3)))
+    assert isinstance(blue_red(np.ndarray((360, 680, 3))), np.ndarray)
