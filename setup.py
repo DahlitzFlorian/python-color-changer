6f266b99f324fcb43dbb9bd327a20a657423a01b
Florian Dahlitz <f2dahlitz@freenet.de>
2018-03-27 22:23:02 +0200

    changed before_install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='color-changer',
-    version='1.0.0',
+    version='1.0.1',
     packages=['colorchanger', ],
     license='MIT',
     description='Reads in an image and swap specified colors ',
